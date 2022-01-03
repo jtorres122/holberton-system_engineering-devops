007c3816c48eb344a34e953d0c154520b4111e4e
Julian <jose.torres122@outlook.com>
2022-01-03 01:00:32 -0400

    Be the first of Configuration management project

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Manifest creates a file
+
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
