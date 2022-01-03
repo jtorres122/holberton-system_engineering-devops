a525bc663a85865df12a42bdb5cbe54b60651912
Julian <jose.torres122@outlook.com>
2022-01-03 16:07:23 -0400

    Attempt to comply by installing the latest version

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,6 +3,6 @@
 package { 'puppet-lint':
   ensure          => '2.5.0',
   provider        => 'gem',
-  source          => 'https://rubygems.org/',
+  source          => 'http://rubygems.org/',
 }
 
