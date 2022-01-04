21ea67b35c5173295b0bc6748d659597cbfcf4ff
Julian <jose.torres122@outlook.com>
2022-01-04 13:25:12 -0400

    Be a YOLO puppet file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,12 @@
 # Manifest makes change to config file
 include stdlib
 
-file_line { 'ssh_config_line':
-  line => 'BatchMode yes
-IdentityFile ~/.ssh/school',
+file_line { 'BatchMode':
+  line => 'BatchMode yes',
+  path => '/etc/ssh/ssh_config',
+}
+
+file_line { 'IdentifyFile':
+  line => 'IdentityFile ~/.ssh/school',
   path => '/etc/ssh/ssh_config',
 }
