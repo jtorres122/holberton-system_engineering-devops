b65a6f8ecb727f04ed13809dcd0a2648a59eaa51
Julian <jose.torres122@outlook.com>
2022-01-31 22:47:44 -0400

    Ugliest code I have ever written

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,13 +8,40 @@ import json
 from requests import get
 from sys import argv
 
+
 if __name__ == "__main__":
 
-    url = "https://jsonplaceholder.typicode.com/users"
     employee_ID = int(argv[1])
-    request = get(url)
-    users = request.text
-    data = json.loads(users)
-    employee_name = data[employee_ID]
+    num_of_tasks = 0
+    num_of_tasks_done = 0
 
-    print("Employee {}".format(employee_name))
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    users_request = get(users_url)
+    users = users_request.text
+    users_data = json.loads(users)
+
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    todos_request = get(todos_url)
+    todos = todos_request.text
+    todos_data = json.loads(todos)
+
+    for count in todos_data:
+        if count['userId'] == employee_ID and\
+          count['completed'] is True:
+            num_of_tasks_done += 1
+            num_of_tasks += 1
+        if count['userId'] == employee_ID and\
+           count['completed'] is False:
+            num_of_tasks += 1
+
+    for d in users_data:
+        if d['id'] == employee_ID:
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(d['name'],
+                          num_of_tasks_done,
+                          num_of_tasks))
+
+    for title in todos_data:
+        if title['userId'] == employee_ID and\
+          title['completed'] is True:
+            print("\t {}".format(title['title']))
