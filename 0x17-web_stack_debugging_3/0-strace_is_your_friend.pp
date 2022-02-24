91e9f99694d888d46901768ea12e8c4bb7653452
Julian <jose.torres122@outlook.com>
2022-02-24 12:03:11 -0400

    Better late than never

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# Manifest fixes typos and provides new paths for system to run appropriately
+
 exec { 'fix typo':
   onlyif  => 'test -e /var/www/html/wp-settings.php',
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
